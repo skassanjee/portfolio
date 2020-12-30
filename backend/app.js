8d61769de7244f4ab517e4cc51ba91fe2887100d
Sahil <56181733+skassanjee@users.noreply.github.com>
2020-12-29 18:08:04 -0700

    second try

--- backend/app.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express')
-const app = express();;
-const port = 5000
-
- app.get('/', (req, res) => res.send('hello satish'));
-
- app.listen(port, () => console.log('listening at port 5000'))
