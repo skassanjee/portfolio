2cdc5e71bef05662892d2b3e3d980567ee15ba24
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-01-04 03:04:52 -0700

    components added

---
+++ src/Components/ProjectsSection/ProjectsSection.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Projects from '../../Components/Projects/Projects.component'
+
+const ProjectsSection = () => {
+    return(
+        <div>
+            <Projects title="Project 1" />
+            <Projects title="Project 2"/>
+            <Projects title="Project 3"/>
+            <Projects title="Project 4"/>
+            <Projects title="Project 5" />
+            <Projects title="Project 6"/>
+        </div>
+    )
+}
+
+export default ProjectsSection
--- src/Pages/Homepage/Homepage.component.jsx
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../Components/NavbarMenu/NavbarMenu.component'
 import Intro from '../../Components/Intro/intro.component.'
+import ProjectsSection from '../../Components/ProjectsSection/ProjectsSection.component'
 
 const Portfolio = () =>{
     return(
         <div>
             <Intro />
+            <ProjectsSection />
         </div>
     )
 }
