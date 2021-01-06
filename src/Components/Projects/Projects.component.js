9539da54bb2ad15ad139b5b8d4ee6b6923241b32
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-01-06 02:09:22 -0700

    experimenting with material ui grid

--- src/Components/Projects/Projects.component.js
+++ src/Components/Projects/Projects.component.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import img from '../../Assets/placeholder.png'
 const Projects = (props) =>{
     return(
+        
         <div>
-            <h1>{props.title} </h1>
+            <h1> {props.title}</h1>
+            <img src={img} />
+            <p> {props. description}</p>
         </div>
     )
 }
--- src/Components/ProjectsSection/ProjectsSection.component.js
+++ src/Components/ProjectsSection/ProjectsSection.component.js
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Projects from '../../Components/Projects/Projects.component'
+import Data from '../../Data/projects.data'
+import './ProjectsSection.component.js'
+import { Grid } from '@material-ui/core';
 
-const ProjectsSection = () => {
-    return(
-        <div>
-            <Projects title="Project 1" />
-            <Projects title="Project 2"/>
-            <Projects title="Project 3"/>
-            <Projects title="Project 4"/>
-            <Projects title="Project 5" />
-            <Projects title="Project 6"/>
-        </div>
-    )
+class ProjectsSection extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            Data
+        }
+    }
+
+
+
+    render(){
+        return <div className="mainContainer">
+                <Grid item xs={12} sm container justify="space-around"
+  alignItems="center">
+                    <div>
+                         <Projects 
+                              title={"Natours"}
+                              description={"A fictional travel website built with ReactJS for the front end, and nodeJS for the backend."} />
+                    </div>    
+
+                    <div>
+                         <Projects 
+                              title={"project 1"}
+                              description={"Some text"} />
+                    </div>     
+
+                      <div>
+                         <Projects 
+                              title={"project 1"}
+                              description={"Some text"} />
+                    </div>
+                </Grid>
+
+                <Grid item xs={12} sm container justify="space-around"
+  alignItems="center">
+                    <div>
+                         <Projects 
+                              title={"project 1"}
+                              description={"Some text"} />
+                    </div>    
+
+                    <div>
+                         <Projects 
+                              title={"project 1"}
+                              description={"Some text"} />
+                    </div>     
+
+                      <div>
+                         <Projects 
+                              title={"project 1"}
+                              description={"Some text"} />
+                    </div>
+                </Grid>
+                 </div>
+    }
 }
 
 export default ProjectsSection
